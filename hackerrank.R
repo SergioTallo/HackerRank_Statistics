760f1d65b3ca5976fae83ef056471fc9feae8fa1
Sergio Tallo Torres <ic19b047@technikum-wien.at>
2021-08-26 10:29:55 +0200

    Day 3.3

--- hackerrank.R
+++ hackerrank.R
@@ -85,5 +85,42 @@ error <- c()
 for (i in 1:length(X)){
   error <- c(error, (mean(X) - X[i]) ** 2)
 }
+
 my_stddev <- round(sqrt(sum(error) / length(X)), 1)
+cat(my_stddev)
+
+# Day 3.1
+# Basic Probability
+# Probability of a single toss of two dices their sum is at most 9
+
+probset <- c(1/36, 2/36, 3/36, 4/36, 5/36, 6/36, 5/36, 4/36)
+
+totalprob <- sum(probset)
+
+# The answer is 5/6
+
+# Day 3.2
+# In a single toss of  fair (evenly-weighted) six-sided dice, 
+# find the probability that the values rolled by each die will 
+# be different and the two dice have a sum of 6.
+
+# P(A) A = Numbers are different
+probdifferent <- 30/36
+# P(B) B = Sum is six
+prob6 <- 5/36
+# P(A|B) A|B = Numbers are different if sum is 6
+probdifferentif6 <- 4/5
+# P(A and B) A and B = Numbers are different and sum is six
+# P(A and B) = P(A|B) * P(B) = 4/5 * 5/6
+probdiffand6 <- probdifferentif6 * prob6
+# Answer is 4/36 = 1/9
+
+
+# Day 3.3
+# There are  urns labeled X, Y, and Z. 
+# Urn X contains 4 red balls and 3 black balls.
+# Urn Y contains 5 red balls and 4 black balls.
+# Urn Z contains 4 red balls and 4 black balls. 
+# One ball is drawn from each of the 3 urns. What is the probability that, 
+# of the 3 balls drawn, 2 are red and 1 is black?
 
