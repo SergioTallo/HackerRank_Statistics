080aeb27e301351b52879a782bb69bc416f0038e
Sergio Tallo Torres <ic19b047@technikum-wien.at>
2021-08-28 15:16:34 +0200

    Statistics Day 5

--- hackerrank.R
+++ hackerrank.R
@@ -133,3 +133,84 @@ probdiffand6 <- probdifferentif6 * prob6
 # rbr = 4/7 * 4/9 * 1/2 = 16/126
 # brr = 3/7 * 5/9 * 1/2 = 15/126
 # Total = rrb + rbr + brr = 51/126 = 17/42
+
+# Day 4.1
+# Conditional probability
+# Suppose a family has  children, one of which is a boy. 
+# What is the probability that both children are boys?
+# Probability of 2 Boy if we know 1 is a Boy
+# P (2B | 1B) = P (2B and 1B) / P(1B)
+# P (2B and 1B) = 1/4
+# P (1/B) = 3/4
+# P (2B | 1B) = 1/4 / 3/4 = 1/3
+
+# Day 4.2
+# You draw 2 cards from a standard 52-card deck without replacing them. 
+# What is the probability that both cards are of the same suit?
+# First card could be any card. So, probability of 1
+# Second card we have 12 possibilities out of 51.
+# P = 1 * 12/51= 12/51
+
+# Day 4.3
+# A bag contains 3 red marbles and 4 blue marbles. 
+# Then, 2 marbles are drawn from the bag, at random, without replacement. 
+# If the first marble drawn is red, what is the probability 
+# that the second marble is blue?
+# # P (2B | 1R) = P (1R and 2B) / P(1R) = 2/7 / 3/7 = 14/21 = 2/3
+
+# Day 5.1
+# Binomial distribution 1
+# The ratio of boys to girls for babies born in Russia is 1,09:1. 
+# If there is 1 child born per birth, what proportion of Russian 
+# families with exactly 6 children will have at least 3 boys?
+# Write a program to compute the answer using the above parameters. 
+# Then print your result, rounded to a scale of 3 decimal places (i.e.,  format)
+
+# f(x)= (n x) * p**x (1-p)**n-x (dbinom)
+# p = O(p) / (1 + O(p)) = 1.09 / (1 + 1.09) (0p = y)
+# n = 6
+# x = 3; x = 4; x= 5; x= 6
+
+arr <- readcommand()
+
+y <- arr[1]
+p <- y/(1 + y)
+n <- 6
+
+total <- c()
+for (x in 3:6){
+  total <- c(total, dbinom(x, n, p))
+}
+
+totalprob <- round(sum(total), 3)
+cat (totalprob)
+
+# Day5.2
+# Binomial distribution 2
+# A manufacturer of metal pistons finds that, on average, 12%  of the pistons 
+# they manufacture are rejected because they are incorrectly sized. 
+# What is the probability that a batch of 10 pistons will contain:
+# No more than 2 rejects?
+# At least 2 rejects?
+
+arr <- readcommand()
+
+p <- arr[1] / 100
+n <- arr[2]
+
+total <- c()
+for (x in 0:2){
+  total <- c(total, dbinom(x, n, p))
+}
+
+end1 <- round(sum(total), 3)
+
+total <- c()
+for (x in 0:1){
+  total <- c(total, dbinom(x, n, p))
+}
+
+end2 <- 1 - (round(sum(total), 3))
+
+cat(end1)
+cat(end2)
