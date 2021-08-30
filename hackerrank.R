e0b1198bc0b6c81f93b6579a2696899d93d69826
Sergio Tallo Torres <ic19b047@technikum-wien.at>
2021-08-30 09:40:16 +0200

    Day 7

--- hackerrank.R
+++ hackerrank.R
@@ -36,7 +36,8 @@ X <- readcommand()
 W <- readcommand()
 
 c <- weighted.mean(X, W)
-h <- sprintf(c, fmt = '%#.1f')
+
+h <- round(c, 1)
 cat(h)
 
 # Day 2.1
@@ -381,3 +382,64 @@ cat("\n")
 cat(prob2)
 cat("\n")
 cat(prob3)
+
+# Statistics Day 7.1
+# The central limit theorem I
+
+# A large elevator can transport a maximum of 9800 pounds. 
+# Suppose a load of cargo containing 49 boxes must be transported via the 
+# elevator. The box weight of this type of cargo follows a distribution with 
+# a mean of 205 pounds and a standard deviation of 15 pounds. 
+# Based on this information, what is the probability that all 49 boxes 
+# can be safely loaded into the freight elevator and transported?
+
+# normal distribution with:
+# (mu = mean * n) and (sigma = stddev *sqrt(n))
+
+arr <- readcommand()
+x <- arr[1]
+
+arr <- readcommand()
+n <- arr[1]
+
+arr <- readcommand()
+mean <- arr[1]
+
+arr <- readcommand()
+stddev <- arr[1]
+
+mu <- mean * n
+sigma <- stddev * sqrt(n)
+
+prob <- round(pnorm(x, mu, sigma), 4)
+cat(prob)
+
+
+# Statistics Day 7.2
+# The central limit theorem II
+
+# The number of tickets purchased by each student for the 
+# University X vs. University Y football game follows a distribution 
+# that has a mean of 2.4 and a standard deviation of 2.0.
+# A few hours before the game starts, 100 eager students line up to purchase 
+# last-minute tickets. If there are only 250 tickets left, 
+# what is the probability that all 100 students will be able to purchase tickets?
+
+arr <- readcommand()
+x <- arr[1]
+
+arr <- readcommand()
+n <- arr[1]
+
+arr <- readcommand()
+mean <- arr[1]
+
+arr <- readcommand()
+stddev <- arr[1]
+
+mu <- mean * n
+sigma <- stddev * sqrt(n)
+
+prob <- round(pnorm(x, mu, sigma), 4)
+cat(prob)
+
